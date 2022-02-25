bb3845b4b3ffd56a8db1924def6fb5aca552d5a1
Porping <porsteam@hotmail.com>
2022-02-25 19:19:18 +0700

    feat:test fetch api in sanity and define type of post for using typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Show from '../components/Show'
+import { sanityCLient, urlFor } from '../sanity'
+import { Post } from './typing'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home: NextPage = ({ posts }: Props) => {
+  console.log(posts)
   return (
     <div className='mx-auto max-w-7xl'>
       <Head>
@@ -18,3 +25,26 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == 'post']{
+    _id,
+    title,
+    slug,
+    author->{
+    name,
+    image
+  },
+  description,
+  mainImage,
+  }`;
+
+  const posts = await sanityCLient.fetch(query);
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
