64f8992dd487d99b60c1dae37bc2030eb883b8f5
Porping <porsteam@hotmail.com>
2022-02-26 19:23:34 +0700

    add PortableText for using in body object sanity

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../typing'
+import PortableText from 'react-portable-text'
 interface Props {
   post: Post
 }
@@ -15,8 +16,45 @@ function Post({ post }: Props) {
         alt=""
         className="object-cover w-full h-40"
       />
-      <article>
-        <h1>{post.title}</h1>
+      <article className="max-w-3xl p-5 mx-auto space-y-3">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500">{post.description}</h2>
+        <div className="flex items-center space-x-2">
+          <img
+            src={urlFor(post.author.image).url()!}
+            alt=""
+            className="w-10 h-10 rounded-full"
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-300">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="">
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => {
+                ;<h1 className="my-5 text-2xl font-bold" {...props} />
+              },
+              h2: (props: any) => {
+                ;<h1 className="my-5 text-xl font-bold" {...props} />
+              },
+              li: ({ children }: any) => {
+                ;<li className="ml-4 list-disc"> {children}</li>
+              },
+              link: ({ href, children }: any) => {
+                ;<a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              },
+            }}
+          />
+        </div>
       </article>
     </main>
   )
@@ -47,7 +85,7 @@ export const getStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const query = `*[_type == 'post'&&slug.current == $slug][0]{
         _id,
-        _createAt,
+        _createdAt,
         title,
         author->{
         name,
